242c2e4585e50e423129dcb95dab7ba5265edd35
simfrisk <simoncarlfrisk@gmail.com>
2025-04-22 09:26:58 +0200

    added some css to the cards

--- src/sections/03-featured-project-section/components/ProjectCard.jsx
+++ src/sections/03-featured-project-section/components/ProjectCard.jsx
@@ -1,30 +1,37 @@
 import "../featured-project-section.css"
+import { ProjectCardContainer } from "../../../global-componets/ProjectCardContainer.jsx"
 
 export const ProjectCard = ({ title, description, languageTag, id, img, livePageLink, gitHubLink }) => {
   return (
     <>
-      <img className="project-img" src={img} alt="" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <div className="language-tag-container">
-        {languageTag.map((lang, id) => (
-          <p className="language-tag" key={id}>{lang}</p>
-        ))}
-      </div>
-      <a href={livePageLink} target="_blank" rel="noopener noreferrer">
-        <button className="web-btn">
-          <img className="web-icon" src="./ic-web.svg" alt="" />
-          Live Demo
-        </button>
-      </a>
-      <a href={gitHubLink} target="_blank" rel="noopener noreferrer">
-        <button className="web-btn">
-          <img className="web-icon" src="./Ic-Github.svg" alt="" />
-          View Code
-        </button>
-      </a>
-      <br />
-      <br />
+      <ProjectCardContainer>
+        <div className="card-img-container">
+          <img className="project-img" src={img} alt="" />
+        </div>
+        <div className="card-content-container">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <div className="language-tag-container">
+            {languageTag.map((lang, id) => (
+              <p className="language-tag" key={id}>{lang}</p>
+            ))}
+          </div>
+          <a href={livePageLink} target="_blank" rel="noopener noreferrer">
+            <button className="web-btn">
+              <img className="web-icon" src="./ic-web.svg" alt="" />
+              Live Demo
+            </button>
+          </a>
+          <a href={gitHubLink} target="_blank" rel="noopener noreferrer">
+            <button className="web-btn">
+              <img className="web-icon" src="./Ic-Github.svg" alt="" />
+              View Code
+            </button>
+          </a>
+        </div>
+        <br />
+        <br />
+      </ProjectCardContainer>
     </>
   )
 }
