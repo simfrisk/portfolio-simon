d700eb7d36e12e53938bc26e87dab4ad376e4470
simfrisk <simoncarlfrisk@gmail.com>
2025-04-22 21:00:25 +0200

    added hide extra card

--- src/sections/03-featured-project-section/FeaturedProjectsSection.jsx
+++ src/sections/03-featured-project-section/FeaturedProjectsSection.jsx
@@ -10,16 +10,18 @@ export const FeaturedProjectSection = () => {
     <>
       <section>
         <H2>Featured Projects</H2>
-        {data.cardData.map((card, id) => (
-          <ProjectCard
-            key={id}
-            title={card.title}
-            description={card.description}
-            languageTag={card.languageTag}
-            img={card.img}
-            livePageLink={card.livePageLink}
-            gitHubLink={card.gitHubLink} />
-        ))}
+        {data.cardData
+          .filter(card => card.displayCard !== "none")
+          .map((card, id) => (
+            <ProjectCard
+              key={id}
+              title={card.title}
+              description={card.description}
+              languageTag={card.languageTag}
+              img={card.img}
+              livePageLink={card.livePageLink}
+              gitHubLink={card.gitHubLink} />
+          ))}
         <Button text={"See more projects"} icon={"./Ic-ArrowDown.svg"} />
       </section>
     </>
