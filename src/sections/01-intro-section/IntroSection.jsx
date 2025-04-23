43c52885a044d9bbe64d51ee1b40d512c635dbd0
simfrisk <simoncarlfrisk@gmail.com>
2025-04-23 13:41:06 +0200

    bug

--- src/sections/01-intro-section/IntroSection.jsx
+++ src/sections/01-intro-section/IntroSection.jsx
@@ -1,7 +1,6 @@
 import { TitleSimonFrisk } from "./components/TitleSimonFrisk"
 import { TileImages } from "./components/TileImages"
 import { WhoAmI } from "./components/WhoAmI"
-import "./introSection.css"
 import { Section } from "../../global-componets/Section"
 
 export const IntroSection = () => {
