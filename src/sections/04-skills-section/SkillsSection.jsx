accba0a6eda09b600f5d74a0b952fcdb3ceb65a8
simfrisk <simoncarlfrisk@gmail.com>
2025-04-23 14:33:15 +0200

    Added new card

--- src/sections/04-skills-section/SkillsSection.jsx
+++ src/sections/04-skills-section/SkillsSection.jsx
@@ -1,20 +1,34 @@
 import data from "./data/skills-data.json"
 import { SkillCategory } from "./components/SkillCategory"
-import "./skills-section.css"
 import { H2 } from "../../global-componets/typography"
 import { Section } from "../../global-componets/Section"
+import styled from "styled-components"
+
+const Skills = styled.div`
+
+  display: flex;
+  flex-direction: column;
+
+@media (min-width: 1024px) {
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 24px;
+}
+`
 
 export const SkillsSection = () => {
   return (
     <>
       <Section secondarySection className="black-section">
         <H2>Skills</H2>
-        <div className="skills">
+        <Skills>
           <SkillCategory title="Code" skills={data.SkillsData.Code} />
           <SkillCategory title="Toolbox" skills={data.SkillsData.Toolbox} />
           <SkillCategory title="Upcoming" skills={data.SkillsData.Upcoming} />
           <SkillCategory title="More" skills={data.SkillsData.More} />
-        </div>
+        </Skills>
       </Section>
     </>
   )
--- src/sections/04-skills-section/components/SkillCategory.jsx
+++ src/sections/04-skills-section/components/SkillCategory.jsx
@@ -1,9 +1,21 @@
 import { H3, P } from "../../../global-componets/typography";
 
+import styled from "styled-components";
+
+export const SkillTag = styled.p`
+  border: solid 2px rgb(255, 255, 255);
+  padding: 6px;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  width: 177px;
+  margin: 0 auto;
+`
+
 export const SkillCategory = ({ title, skills }) => {
   return (
     <div>
-      <H3 className="skill-title">{title}</H3>
+      <SkillTag className="skill-title">{title}</SkillTag>
       {
         skills.map((skill, index) => (
           <P key={index}>{skill}</P>
